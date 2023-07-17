e2a0db9e8ee49e21c9ddfd4c4ce8bb53e8663191
Artur-Barreto <artur.barreto7@outlook.com>
2023-07-17 14:15:59 -0300

    testando o github pages

--- main.tf
+++ main.tf
@@ -1,32 +1,24 @@
 data "aws_ami" "ubuntu" {
-    most_recent = true
+  most_recent = true
 
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-    
-    owners = ["099720109477"] # Canonical
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-provider "aws" {
-  region  = "us-east-1"
-}
-
-resource "aws_instance" "app_server" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "app-ssh-key"
 
   tags = {
-    Name = var.ec2_name
+    Name = "HelloWorld"
   }
 }
-
-
-
