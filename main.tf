235f3ab20f1ca560baf7a140235f38ce4164a25e
Artur-Barreto <artur.barreto7@outlook.com>
2023-07-17 21:15:34 -0300

    testando

--- main.tf
+++ main.tf
@@ -1,24 +1,26 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
+  backend "s3" {
+    bucket = "sandbox-terraform-state-sandbox-us-east"
+    key = "prod/terraform.tfstate"
+    region = "us-east-1"
   }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+}
+
+
+
